03e57163b69deef5db6a2bbd288ab3e5da205ea9
Christian Grimberg <christian.grimberg@outlook.com>
2021-09-13 22:48:02 +0000

    :+1: Practice with DDL.

---
+++ db/curso_lunes/7.DDL.sql
@@ -0,0 +1,52 @@
+USE curso_lunes;
+
+SHOW TABLES;
+
+-- DDL - Lenguage de Definicion de Datos (Data Definition Language)
+
+-- Create table
+CREATE TABLE IF NOT EXISTS gente (
+    nombre VARCHAR(60)
+    ,fecha DATE
+);
+DESCRIBE gente;
+
+
+-- Rename [gente] by [personas]
+ALTER TABLE curso_lunes.gente RENAME personas;
+DESCRIBE curso_lunes.personas;
+
+-- Add a field to table
+ALTER TABLE curso_lunes.personas ADD comentario VARCHAR(100);
+DESCRIBE curso_lunes.personas;
+
+-- Change data type of a field
+USE curso_lunes;
+ALTER TABLE personas MODIFY COLUMN comentario VARCHAR(150);
+DESCRIBE personas;
+
+-- Other method to change data type of a field
+ALTER TABLE curso_lunes.personas CHANGE comentario observacion VARCHAR(200);
+ALTER TABLE curso_lunes.personas MODIFY COLUMN observacion VARCHAR(250);
+DESCRIBE curso_lunes.personas;
+
+-- Remove a field
+ALTER TABLE curso_lunes.personas DROP observacion;
+DESCRIBE curso_lunes.personas;
+
+/*
+-- Agregar los campos DNI y Telefono
+-- cambiar el campo nombre por apellido VARCHAR(60)
+-- borrar el campo fecha
+*/
+ALTER TABLE curso_lunes.personas
+    ADD dni INT
+    ,ADD telefono INT
+    ,CHANGE nombre apellido VARCHAR(60)
+    ,DROP fecha;
+DESCRIBE curso_lunes.personas;
+
+-- Remove table
+DROP TABLE curso_lunes.personas;
+USE curso_lunes;
+SHOW TABLES;
