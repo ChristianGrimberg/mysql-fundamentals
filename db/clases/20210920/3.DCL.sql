ed1ef5b560fb5b0d73d255582607701510b79d0c
Christian Grimberg <christian.grimberg@outlook.com>
2021-09-20 22:35:49 +0000

    :+1: Start with DCL.

--- db/clases/20210920/3.DCL.sql
+++ db/clases/20210920/3.DCL.sql
@@ -3,3 +3,13 @@ SHOW TABLES;
 
 -- DCL (Lenguaje de Control de Datos)
 SELECT CURRENT_USER();
+
+-- CREAR UN USUARIO
+DROP USER IF EXISTS mylo@localhost;
+CREATE USER mylo@localhost IDENTIFIED BY '1234';
+
+-- OTORGAR PERMISOS DE LECTURA
+GRANT SELECT ON curso_lunes.clientes TO mylo@localhost;
+
+-- 
+FLUSH PRIVILEGES;
